a933557670818d4aea350ad96919458bbe044150
Artur Alves <artur.alvs00@gmail.com>
2016-09-08 21:43:44 -0300

    Popular o bd e testar tudo + design.

--- p_app/perfil/models.py
+++ p_app/perfil/models.py
@@ -20,12 +20,18 @@ class Politico(models.Model):
 	sobre_nome = models.CharField(max_length=100)
 	idade = models.IntegerField(default=0)
 
+	# Foto do politico
+	imagem = models.CharField(max_length=200, default="-")
+
 
 	# Atributos de Politico ---------------------
 	cargo_atual = models.ForeignKey(Cargo, on_delete=models.CASCADE, blank=True, null=True)
 	# inicio_cargo = models.DateTimeField('Inicio do cargo atual')
 
 	partido_atual = models.ForeignKey(Partido, on_delete=models.CASCADE, blank=True, null=True)
+
+	# Colocar uma foreignKey pra uma busca mais rapida? Nao sei!
+	# candidatura_atual = models.ForeignKey(Candidatura, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Candidatura(models.Model):
@@ -35,8 +41,17 @@ class Candidatura(models.Model):
 
 	eleito = models.BooleanField(default=True)
 
+	id_eleitoral = models.IntegerField(default=0)
+
+
 	inicio_periodo = models.DateTimeField('Inicio da Candidatura')
 	final_periodo = models.DateTimeField('Final da Candidatura')
+
+	# Poderia ser 2
+	estado = models.CharField(max_length=20, default="-")
+
+	# Total de doacao recebida na Candidatura
+	total_doacao = models.IntegerField(default=0)
 
 
 class Projeto(models.Model):
@@ -48,3 +63,17 @@ class Projeto(models.Model):
 	data_andamento = models.DateTimeField('Data do andamento atual')
 
 	criador = models.ForeignKey(Politico, on_delete=models.CASCADE)
+
+
+class Doacao(models.Model):
+	doador = models.CharField(max_length=200)
+	valor  = models.IntegerField(default=0)
+
+	politico = models.ForeignKey(Politico, on_delete=models.CASCADE)
+
+class Bancada(models.Model):
+	titulo = models.CharField(max_length=200)
+
+class Colecao_Bancada(models.Model):
+	bancada = models.ForeignKey(Bancada, on_delete=models.CASCADE, blank=True, null=True)
+	politico = models.ForeignKey(Politico, on_delete=models.CASCADE)
--- p_app/perfil/views.py
+++ p_app/perfil/views.py
@@ -17,6 +17,7 @@ def index(request):
 
 	return HttpResponse(template.render(None, request))
 
+# Politicos ----------------------------------------------------------------------------------
 def lista_politicos(request):
 
 	politico_query_result = Politico.objects.order_by('-id')[:5]
@@ -39,16 +40,7 @@ def detalhes_politico(request, id_politico):
 
 	return render(request, 'politico/detalhes_politico.html', {'politico': politico})
 
-def detalhes_partido(request, id_partido):
-	# try:
-	# 	question = Question.objects.get(pk=q_id)
-	# except Question.DoesNotExist:
-	# 	raise Http404("Question does not exist")
-
-	partido = get_object_or_404(Partido, pk=id_partido)
-
-	return render(request, 'politico/detalhes_partido.html', {'partido': partido})
-
+# Partidos ----------------------------------------------------------------------------------
 def lista_partidos(request):
 	partidos_query_result = Politico.objects.order_by('-id')[:5]
 	template = loader.get_template('politico/partidos.html')
@@ -59,3 +51,13 @@ def lista_partidos(request):
 	# output = ', '.join([q.question_text for q in latest_question_list])
 	return HttpResponse(template.render(context, request))
 	# return HttpResponse("Hello, world. Polls index.")
+
+def detalhes_partido(request, id_partido):
+	# try:
+	# 	question = Question.objects.get(pk=q_id)
+	# except Question.DoesNotExist:
+	# 	raise Http404("Question does not exist")
+
+	partido = get_object_or_404(Partido, pk=id_partido)
+
+	return render(request, 'politico/detalhes_partido.html', {'partido': partido})
